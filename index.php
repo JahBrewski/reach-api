f5139e634cffc42814c5585dda6466bb911a325e
Brook Davis <mrgoby@gmail.com>
2015-11-18 22:58:37 +0000

    major cleanup and jwt finalization.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 ####################### CLASSES #######################
+//@todo  autoload these
 class User extends \Illuminate\Database\Eloquent\Model
 {
   protected $table = 'users';
@@ -46,16 +47,32 @@ class Message extends \Illuminate\Database\Eloquent\Model
   protected $fillable = array(
                           'company_id',
                           'from_user_id',
-                          'recipient_user_id', 'message_content', 'timestamp_queued',' timestamp_dequeued');
+                          'recipient_user_id', 
+								  'message_content', 
+								  'timestamp_queued',
+								  'timestamp_dequeued'
+								  );
 }
 
+class Bulletin extends \Illuminate\Database\Eloquent\Model
+{
+  protected $table = 'bulletins';
+  public $timestamps = false;
+  protected $fillable = array(
+                          'company_id',
+                          'from_user_id',
+                          'message_content', 
+								  'timestamp_queued',
+								  'timestamp_dequeued'
+								  );
+}
 
 //setup logging
-###n$monolog = new \Flynsarmy\SlimMonolog\Log\MonologWriter(array(
-###n    'handlers' => array(
-###n        new \Monolog\Handler\StreamHandler('./logs/'.date('Y-m-d').'.log'),
-###n    ),
-###n));
+$monolog = new \Flynsarmy\SlimMonolog\Log\MonologWriter(array(
+    'handlers' => array(
+        new \Monolog\Handler\StreamHandler('./logs/'.date('Y-m-d').'.log'),
+    ),
+));
 
 /**
  * Configure the database and boot Eloquent
@@ -71,59 +88,44 @@ $capsule->addConnection(array(
     'collation' => 'utf8_general_ci',
     'prefix'    => ''
 ));
+
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
 
-
 // Create Slim app
 $app = new \Slim\Slim(array(
-'log.enabled' =>    true,
-'log.level' =>      \Slim\Log::DEBUG,
+    'log.enabled' =>    true,
+    'log.level' =>      \Slim\Log::DEBUG,
+    'log.writer' => $monolog
 ));
-
-$app->container->singleton('log', function () {
-    $log = new \Monolog\Logger();
-    $log->pushHandler(new \Monolog\Handler\StreamHandler('./logs/log.txt'));
-    return $log;
-});
 
 //setup jwt auth
 $app->add(new \Slim\Middleware\JwtAuthentication([
-"secure" => false,
-"secret" => "supersecretkeyyoushouldnotcommittogithub",
+    "secure" => false,
+    "secret" => "supersecretkeyyoushouldnotcommittogithub",
+    "callback" => function ($options) use ($app) {
+            $app->jwt = $options["decoded"];
+	               },
 #'logger' => $monolog,
-"rules" => [
-  new \Slim\Middleware\JwtAuthentication\RequestPathRule([
-    "path" => "/",
-    "passthrough" => array("/login")
-	 ]),
-  new \Slim\Middleware\JwtAuthentication\RequestMethodRule([
-    "passthrough" => ["OPTIONS"]
-    ])
-	 ]
+    "rules" => [
+      new \Slim\Middleware\JwtAuthentication\RequestPathRule([
+        "path" => "/",
+        "passthrough" => array("/login")
+    	 ]),
+      new \Slim\Middleware\JwtAuthentication\RequestMethodRule([
+        "passthrough" => ["OPTIONS"]
+        ])
+    	 ]
 ]));
 
 
-#########################
-######## Routes #########
-#########################
+#################################################
+#################### Routes #####################
+#################################################
 
-$app->get('/foo', function () {
-    // Fetch all books
-    $books = \Book::all();
-    echo $books->toJson();
-
-    // Or create a new book
-    $book = new \Book(array(
-        'title' => 'Sahara',
-        'author' => 'Clive Cussler'
-    ));
-    $book->save();
-});
-
-//XXX put a if DEVMODE around this.
-$app->get('/create-db-schema', function () {
-  create_reach_schema();
+//accept all options
+$app->options('/(:name+)', function() use($app) {
+    $app->response->setStatus(200);
 });
 
 $app->get('/', function() use($app) {
@@ -134,16 +136,8 @@ $app->get('/', function() use($app) {
 $app->post('/login', function() use($app) {
     $app->response->setStatus(200);
     doLogin();
-
 });
 
-$app->options('/login', function() use($app) {
-    $app->response->setStatus(200);
-});
-
-
-
-#CRUD
 $app->get('/user', function() use($app) {
     $app->response->setStatus(200);
     $users = \User::all();
@@ -151,7 +145,9 @@ $app->get('/user', function() use($app) {
 });
 
 $app->get('/user/:uid', function($uid) use($app) {
+    $user = \User::find($uid);
     $app->response->setStatus(200);
+    echo $user->toJson();
 });
 
 $app->post('/user', function() use($app) {
@@ -163,67 +159,69 @@ $app->get('/company', function() use($app) {
     $companies = \Company::all();
     echo $companies->toJson();
 });
-$app->options('/company', function() use($app) {
-    $app->response->setStatus(200);
-});
-
 /*
 $app->get('/company/:cid', function($cid) use($app) {
     $app->response->setStatus(200);
 });
 */
+$app->get('/message/:uid', function($uid) use($app) {
+    $app->response->setStatus(200);
+	 $req_user = $app->jwt->data->userId;
+    $messages = \Message::orderBy('timestamp_queued','ASC')
+	 ->whereRaw('(from_user_id=? OR recipient_user_id=?) AND (from_user_id=? OR recipient_user_id=?)', 
+	   [$uid,$uid,$req_user,$req_user])
+	 ->get();
+	 echo $messages->toJson();
+});
 
 $app->post('/message', function() use($app) {
     $app->response->setStatus(200);
+	 $posty = $app->request->post();
+	 $message = new \Message();
+	 $message->company_id = '1'; //XXX bad
+	 $message->from_user_id = $posty['sender_uid'];
+	 $message->recipient_user_id = $posty['recipient_uid'];
+	 $message->message_content = $posty['message_content'];
+	 $message->timestamp_queued = date('Y-m-d h:i:s');
+	 $message->timestamp_dequeued = date('Y-m-d h:i:s');
+	 $message->save();
+
+    //XXX XXX XXX
+    $app_id = '140562';
+    $app_key = '95129dfbfbc16ec4a811';
+    $app_secret = '1a5dac7bf5d8f1fd9c33';
+    $pusher = new Pusher( $app_key, $app_secret, $app_id );
+    $pusher->trigger( 'my_channel'.$posty['recipient_uid'], 'my_event', $posty['message_content']);
+
+});
+
+$app->get('/feed/:uid', function($uid) use($app) {
+    $app->response->setStatus(200);
+    $all_messages = \Message::orderBy('timestamp_queued','DESC')
+	 ->whereRaw('from_user_id=? OR recipient_user_id=?', 
+	   [$uid,$uid])
+    ->get();
+	 $feed = array();
+	 $contacts = array();
+    foreach($all_messages as $message){
+      $contact = ($message->recipient_user_id !== $uid) ? 
+		  $message->recipient_user_id : $message->from_user_id;
+		if(isset($contacts[$contact])){
+        continue;
+		}
+      $contacts[$contact] = TRUE;
+		$feed[] = $message;
+    }
+    echo json_encode($feed);
 });
 
 #END CRUD
-
-
+//XXX..
+$app->get('/create-db-schema', function () {
+  create_reach_schema();
+});
 
 $app->run();
-
-####################### DATABASE HELPERS ###################
-function create_reach_schema() {
-
-  Capsule::schema()->dropIfExists('users');
-  Capsule::schema()->create('users', function ($table) {
-      $table->increments('id');
-      $table->string('email');
-      $table->string('encrypted_password');
-      $table->string('first_name');
-      $table->string('last_name');
-  });
-
-  Capsule::schema()->dropIfExists('companies');
-  Capsule::schema()->create('companies', function ($table) {
-      $table->increments('id');
-      $table->string('name');
-      $table->string('description');
-  });
-
-  Capsule::schema()->dropIfExists('perms');
-  Capsule::schema()->create('perms', function ($table) {
-      $table->increments('id');
-      $table->string('company_id');
-      $table->string('user_id');
-      $table->string('role'); //enumerate
-  });
-
-  Capsule::schema()->dropIfExists('messages');
-  Capsule::schema()->create('messages', function ($table) {
-      $table->increments('id');
-      $table->string('company_id');
-      $table->string('from_user_id');
-      $table->string('recipient_user_id');
-      $table->string('message_content');
-      $table->string('timestamp_queued'); //enumerate
-      $table->string('timestamp_dequeued'); //enumerate
-  });
-
-}
-
-
 
 ############################################
 ############ Utility functions #############
@@ -265,15 +263,13 @@ function doLogin() {
                     $expire     = $notBefore + 3600; // Adding 60 seconds
                     $serverName = $config['serverName'];
 
-                    $roles = getRoles($user->id);
-						  $companies = array();
+						  $roles = \Perm::where('user_id', '=', $user->id)->get();
+                    $indexed_roles = array();
 						  if(sizeof($roles) > 0){
-						    foreach($roles as $role){
-                         $company = \Company::where('id', '=', $role->company_id)->get();
-								 $companies[] = $company[0];
+                      foreach($roles as $role){
+                        $indexed_roles[] = $role;
 							 }
-                    }
-
+						  }
 
                     /*
                      * Create the token as an array
@@ -287,8 +283,7 @@ function doLogin() {
                         'data' => array(                  // Data related to the signer user
                             'userId'   => $user->id, // userid from the users table
                             'userName' => $username, // User name
-                            'roles' => $roles,
-									 'companies' => $companies
+                            'roles' => $indexed_roles
                         )
                     );
                     
@@ -340,173 +335,46 @@ function doLogin() {
     }
 }
 
-function getPusher() {
+####################### DATABASE HELPERS ###################
+function create_reach_schema() {
 
-    $app_id = '140562';
-    $app_key = '95129dfbfbc16ec4a811';
-    $app_secret = '1a5dac7bf5d8f1fd9c33';
+  Capsule::schema()->dropIfExists('users');
+  Capsule::schema()->create('users', function ($table) {
+      $table->increments('id');
+      $table->string('email');
+      $table->string('encrypted_password');
+      $table->string('first_name');
+      $table->string('last_name');
+  });
 
-    $pusher = new Pusher( $app_key, $app_secret, $app_id );
+  Capsule::schema()->dropIfExists('companies');
+  Capsule::schema()->create('companies', function ($table) {
+      $table->increments('id');
+      $table->string('name');
+      $table->string('description');
+  });
 
-    return $pusher;
+  Capsule::schema()->dropIfExists('perms');
+  Capsule::schema()->create('perms', function ($table) {
+      $table->increments('id');
+      $table->string('company_id');
+      $table->string('user_id');
+      $table->string('role'); //enumerate
+  });
+
+  Capsule::schema()->dropIfExists('messages');
+  Capsule::schema()->create('messages', function ($table) {
+      $table->increments('id');
+      $table->string('company_id');
+      $table->string('from_user_id');
+      $table->string('recipient_user_id');
+      $table->string('message_content');
+      $table->string('timestamp_queued'); //enumerate
+      $table->string('timestamp_dequeued'); //enumerate
+  });
+
 }
-### 
-### 
-### function getAllUsers() {
-###     $sql = "SELECT * FROM users";
-###     try {
-###         $db = getDBConnection();
-###         $stmt = $db->prepare($sql);
-###         $stmt->execute();
-###         $users = $stmt->fetchAll(PDO::FETCH_OBJ);
-###         $db = null;
-###         echo '{"users": ' . json_encode($users) . '}';
-###     } catch(PDOException $e) {
-###         echo '{"error":{"text":'. $e->getMessage() .'}}';
-###     }
-### }
-### 
-### function getUser($uid) {
-###     $sql = "SELECT * FROM users WHERE uid=:uid";
-###     try {
-###         $db = getDBConnection();
-###         $stmt = $db->prepare($sql);
-###         $stmt->bindParam("uid", $uid);
-###         $stmt->execute();
-###         $user = $stmt->fetchAll(PDO::FETCH_OBJ);
-###         $db = null;
-###         echo json_encode($user[0]);
-###     } catch(PDOException $e) {
-###         echo '{"error":{"text":'. $e->getMessage() .'}}';
-###     }
-### }
-### 
-### function createUser() {
-### 
-###     //xxx should check if user already exists
-### 
-###     $sql = "INSERT INTO users
-###             ('email','encrypted_password','first_name','last_name') 
-###             VALUES 
-###             (:email,:encrypted_password,:first_name,:last_name)";
-###     try {
-###         $db = getDBConnection();
-###         $stmt = $db->prepare($sql);
-###         $stmt->bindParam("uid", $uid);
-###         $stmt->execute();
-###         $db = null;
-###     } catch(PDOException $e) {
-###         echo '{"error":{"text":'. $e->getMessage() .'}}';
-###     }
-### }
-### 
-### function updateUser() {}
-### 
-### function getAllEmployees($cid) {
-### 
-###     $sql = "SELECT u.uid,u.first_name,u.last_name FROM users u, perms p WHERE p.cid=:cid AND u.uid=p.uid AND p.role='employee'";
-###     try {
-###         $db = getDBConnection();
-###         $stmt = $db->prepare($sql);
-###         $stmt->bindParam("cid", $cid);
-###         $stmt->execute();
-###         $user = $stmt->fetchAll(PDO::FETCH_OBJ);
-###         $db = null;
-###         echo json_encode($user);
-###     } catch(PDOException $e) {
-###         echo '{"error":{"text":'. $e->getMessage() .'}}';
-###     }
-### }
-### 
-### function getAllCustomers($cid) {
-### 
-###     $sql = "SELECT u.uid,u.first_name,u.last_name FROM users u, perms p WHERE p.cid=:cid AND u.uid=p.uid AND p.role='customer'";
-###     try {
-###         $db = getDBConnection();
-###         $stmt = $db->prepare($sql);
-###         $stmt->bindParam("cid", $cid);
-###         $stmt->execute();
-###         $user = $stmt->fetchAll(PDO::FETCH_OBJ);
-###         $db = null;
-###         echo json_encode($user);
-###     } catch(PDOException $e) {
-###         echo '{"error":{"text":'. $e->getMessage() .'}}';
-###     }
-### }
-### 
-### function createMessage() {
-### 
-###     //insert the message into the database as unsent
-###     $sql = "INSERT INTO messages 
-###             (sender_uid,recipient_uid,message_content,timestamp_sent,timestamp_received) 
-###             VALUES 
-###             (:sender_uid,:recipient_uid,:message_content,:timestamp_sent,:timestamp_received)";
-###     //xxx validate data
-###     $sender_uid = $_POST['sender_uid'];
-###     $recipient_uid = $_POST['recipient_uid'];
-###     $message_content = $_POST['message_content'];
-###     $current_time = time();
-###     $received_time = 0;
-### 
-###     try {
-###         $db = getDBConnection();
-###         $stmt = $db->prepare($sql);
-###         $stmt->bindParam("sender_uid", $sender_uid);
-###         $stmt->bindParam("recipient_uid", $recipient_uid);
-###         $stmt->bindParam("message_content", $message_content);
-###         $stmt->bindParam("timestamp_sent", $current_time);
-###         $stmt->bindParam("timestamp_received", $received_time);
-###         $stmt->execute();
-###         $db = null;
-###     } catch(PDOException $e) {
-###         echo '{"error":{"text":'. $e->getMessage() .'}}';
-###     }
-### 
-###     //queue the message for delivery / deliver the message
-###     $data = array('name' => $sender_uid, 'message' => $message_content);
-###     $pusher = getPusher();
-###     $pusher->trigger( 'my_channel'.$recipient_uid, 'my_event', $data );
-### 
-### }
-### 
-### function getMessages($uid,$time=NULL) {
-### 
-### }
-### 
-### function getAllCompanies() {
-###     $sql = "SELECT * FROM companies";
-###     try {
-###         $db = getDBConnection();
-###         $stmt = $db->prepare($sql);
-###         $stmt->execute();
-###         $companies = $stmt->fetchAll(PDO::FETCH_OBJ);
-###         $db = null;
-###         echo json_encode($companies);
-###     } catch(PDOException $e) {
-###         echo '{"error":{"text":'. $e->getMessage() .'}}';
-###     }
-### }
-### 
-### function getCompany($cid) {
-### 
-###     $sql = "SELECT * FROM companies WHERE cid=:cid";
-###     try {
-###         $db = getDBConnection();
-###         $stmt = $db->prepare($sql);
-###         $stmt->bindParam("cid", $cid);
-###         $stmt->execute();
-###         $company = $stmt->fetchAll(PDO::FETCH_OBJ);
-###         $db = null;
-###         echo json_encode($company[0]);
-###     } catch(PDOException $e) {
-###         echo '{"error":{"text":'. $e->getMessage() .'}}';
-###     }
-### 
-### }
-### 
-### 
-### function createBulletin() {}
-### function getBulletins($uid,$time=NULL) {}
+
 ### 
 ### function sendInvite() {}
 ### function processInviteAck() {}
@@ -514,20 +382,4 @@ function getPusher() {
 ### function sendRequest() {}
 ### function processRequestAck() {}
 ### 
-### 
-function getRoles($user_id) {
-  $roles = \Perm::where('user_id', '=', $user_id)->get();
-  return $roles;
-}
-### 
-### 
-### function getDBConnection() {
-###     $dbhost="127.0.0.1";
-###     $dbuser="reachdb";
-###     $dbpass="reachdb123";
-###     $dbname="reach_app";
-###     $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-###     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-###     return $dbh;
-### }
 ### 
